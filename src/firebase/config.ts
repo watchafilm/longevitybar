83819b0444824caa830dc4cd164ea7d2d1ddb8ee
Watcharapon <watcharapon.pck@gmail.com>
2025-10-22 07:51:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7526057076-110b4",
+  "appId": "1:173646750631:web:9d69c8397c8409930a0c82",
+  "apiKey": "AIzaSyD_Rfw90cHuUsazzIy4EN3YFpEcLa0uGkU",
+  "authDomain": "studio-7526057076-110b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "173646750631"
+};
