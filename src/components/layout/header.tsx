84cba0691bcfb0080003f3419cb71874da71c431
Watcharapon <watcharapon.pck@gmail.com>
2025-10-22 09:28:49 +0000

    เปลี่ยนไอค่อนเป็นลิ้ง https://drive.google.com/uc?export=view&id=1Pwf3Rx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ export function AppHeader() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-14 items-center justify-center">
         <Link href="/" className="flex items-center space-x-2">
-          <Image src="https://pic.onlinewebfonts.com/thumbnails/icons_285144.svg" alt="Longevity Bar Logo" width={40} height={40} className="h-10 w-10 filter invert-[.25] sepia-[1] saturate-[5] hue-rotate-[20deg]" />
+          <Image src="https://drive.google.com/uc?export=view&id=1Pwf3RxSHPaugo7mvNVmngr4PZmSnUvOa" alt="Longevity Bar Logo" width={40} height={40} className="h-10 w-10" />
           <h1 className="bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text font-headline text-4xl font-bold text-transparent">
             Longevity Bar
           </h1>
