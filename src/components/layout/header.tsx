ae5a4a0d8d486c94f246b91e2b4ba4953c288f9c
Watcharapon <watcharapon.pck@gmail.com>
2025-10-22 08:33:18 +0000

    เพิ่มขนาดไอค่อน + เปลี่ยนเป็นสีทอง

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,7 +8,7 @@ export function AppHeader() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex h-14 items-center justify-center">
         <Link href="/" className="flex items-center space-x-2">
-          <Image src="https://pic.onlinewebfonts.com/thumbnails/icons_285144.svg" alt="Longevity Bar Logo" width={24} height={24} className="h-6 w-6" />
+          <Image src="https://pic.onlinewebfonts.com/thumbnails/icons_285144.svg" alt="Longevity Bar Logo" width={32} height={32} className="h-8 w-8 filter invert-[.25] sepia-[1] saturate-[5] hue-rotate-[40deg]" />
           <h1 className="font-headline font-bold text-3xl">
             <span className="bg-gradient-to-r from-purple-400 to-purple-600 text-transparent bg-clip-text">Lo</span>
             <span className="bg-gradient-to-r from-blue-400 to-blue-600 text-transparent bg-clip-text">ng</span>
