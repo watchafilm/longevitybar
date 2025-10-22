02c07ca97431b591f6f3a3a46b27a7678d8c7cf7
Watcharapon <watcharapon.pck@gmail.com>
2025-10-22 09:36:29 +0000

    ขวานิดนึง

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,7 @@ export function AppHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center justify-center">
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 pl-20">
           <Link href="/" className="flex items-center space-x-2">
             <Image src="https://drive.google.com/uc?export=view&id=1Pwf3RxSHPaugo7mvNVmngr4PZmSnUvOa" alt="Longevity Bar Logo" width={40} height={40} className="h-10 w-10" />
             <h1 className="bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text font-headline text-2xl md:text-3xl font-bold text-transparent">
